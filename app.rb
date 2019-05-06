81b674de24840f12753a2f158a5deee89df0d1b2
David Acosta <davidacosta25081@yahoo.com>
2019-05-06 22:49:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+get '/name' do 
+  "My name is David"
 end
+
+
+get '/hometown' do 
+  "My hometown is Medellín"
+end
+
+
+get '/favorite-song' do 
+  "My favorite song is We are the champions"
+end
+
+
+
+end 
+
